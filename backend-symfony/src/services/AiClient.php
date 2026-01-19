fcc7c9c02dd09b90c4dae2035c89db95b34a2ac4
De Lima David <davdelima71@gmail.com>
2026-01-19 20:22:32 +0100

    feat: Ajouter le service AiClient pour interagir avec l'API de l'IA

---
+++ backend-symfony/src/services/AiClient.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+
+# This service is used to interact with the API endpoint of the python AI service
+final readonly class AiClient
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private string $aiEndpoint,
+    ) {}
+
+    /**
+    * Sends text to the AI service for analysis and returns the response.
+    * @param string $text The text to be analyzed.
+    * @return string The analysis result from the AI service.
+    * @throws \RuntimeException if there is an error communicating with the AI service.
+    */
+   public function analyse(string $ticket): array
+   {
+        try {
+            $response = $this->httpClient->request(
+                method: 'POST',
+                url: $this->aiEndpoint,
+                options: [
+                    'json' => [
+                        'ticker' => $ticket
+                    ],
+                    'timeout' => 10,
+                ]
+            );
+
+            return $response->toArray(false);
+
+        } catch (
+            TransportExceptionInterface |
+            ClientExceptionInterface |
+            ServerExceptionInterface $e) {
+            // Log the exception or handle it as needed
+            throw new \RuntimeException(
+                message: 'Error communicating with AI service: ' . $e->getMessage(),
+                previous: $e
+            );
+        }
+   }
+}
